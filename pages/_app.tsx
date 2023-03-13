6fa30b36550969024520b5861697db5393641099
arahiko-ayami <zinang7@gmail.com>
2023-03-13 21:31:25 +0700

    add: basic layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import createQueryClient from "../core/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import LoginLayout from "../layouts/Login.layout";
+import PageLayout from "../layouts/Page.layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -17,9 +19,21 @@ export default function App({ Component, pageProps }: AppProps) {
 
   const queryClient = createQueryClient(queryClientHandlers);
 
+  if (router.pathname === "/login") {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <LoginLayout>
+          <Component {...pageProps} />
+        </LoginLayout>
+      </QueryClientProvider>
+    );
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
     </QueryClientProvider>
   );
 }
