f46c82f008bbef31d5b68a1cbe8103ababb53890
ngosangns <ngosangns@gmail.com>
2023-05-15 21:42:56 +0700

    fix(user): use lazy fetch

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import { getAllCommittees } from "../../repositories/Commitee.repository";
@@ -23,7 +23,7 @@ function App() {
 
   const queryClient = useQueryClient();
 
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: ["users", currentPage],
     queryFn: () =>
       getAllUsers({
@@ -31,7 +31,14 @@ function App() {
         page: currentPage,
         limit: currentLimit,
       }),
+    enabled: false,
   });
+
+  useEffect(() => {
+    return () => {
+      refetch();
+    };
+  }, []);
 
   // --- Delete user
   const [deleteUserId, setDeleteUserId] = useState(0);
