5155df4d72a44358fc79fd3d373f9e06ab105cc8
Giap <togiap.dev@gmail.com>
2023-06-21 23:24:30 +0700

    update: role-table

--- pages/roles/index.tsx
+++ pages/roles/index.tsx
@@ -1,75 +1,192 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient} from '@tanstack/react-query';
+import {getAllRoles, createRole, deleteRole} from "@/repositories/Role.repository";
+ 
 import Pagination from "@/components/Pagination";
-
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-function RoleTable() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const { isFetching, error, data, refetch } = useQuery({
-    queryKey: ["repoData", currentPage],
-    queryFn: () =>
-      fetch(`https://kit-api.ngosangns.com/api/users?page=${currentPage}`).then(
-        (res) => res.json()
-      ),
-  });
 
-  const onPageChange = (pageNumber: any) => {
-    setCurrentPage(pageNumber);
-  };
+function TableRole() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ['roles'],
+    queryFn: async () => {
+      const limit = 5
+      const params = {
+        page: currentPage,
+        limit: limit,
+      }
+      return await getAllRoles(params)
+    }
+  })
+
+  const pageData = data
 
   useEffect(() => {
     refetch();
-  }, [currentPage, refetch]);
+  }, [currentPage]);
 
-  if (isFetching) return "Loading...";
+  const handlePageChange =  async (page: any) => {
+    setCurrentPage(page)
+  }
 
-  if (error) return "An error has occurred: " + (error as any)?.message;
+  const queryClient = useQueryClient();
+
+  const [num, setNum] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const handleDelete = async (id: any) => {
+    try {
+      await deleteRole(id);
+      setNum(0);
+      queryClient.invalidateQueries({ queryKey: ['roles'] });
+      refetch();
+    } catch (err) {
+      alert("Đã xảy ra lỗi");
+      console.log(err);
+    }
+  };
+
+  const handleAdd = async (value: string) => {
+    try {
+      await createRole({
+        name: value,
+      });
+      setCount(0);
+      queryClient.invalidateQueries({ queryKey: ['roles'] });
+      refetch();
+    } catch (err) {
+      alert("Đã xảy ra lỗi");
+      console.log(err);
+    }
+  }
+
+  const [valueRole, setValueRole] = useState("");
+
+  const showBtn = () => {
+    if(num === 0) {
+      setNum(1);
+    }
+
+    if(num === 1) {
+      setNum(0);
+    }
+  }
+
+  const showAddForm = () => {
+    if(count === 0) {
+      setCount(1);
+    }
+
+    if(count === 1) {
+      setCount(0);
+    }
+  }
+  
+
+  if (isLoading) return <div>Loading...</div>;
+
+
+
+  if (error) {
+    if (typeof error === 'object' && 'message' in error) {
+      console.log(error.message);
+    } else {
+      console.log('An error has occurred');
+    }
+  }
+  
+
   return (
-    <section id="detais_member_role" data-theme="dark">
-      <div className="detais_member_rolecontainer mx-auto py-5">
-        <div className="title_member_role ml-5">
-          <h1>
-            <span>Tên role: </span>
-            Điều hành
-          </h1>
-          <h2>Danh sách user thuộc role</h2>
-        </div>
-        <div className="overflow-x-auto my-3">
-          <table className="table table-normal w-full text-center">
-            <thead>
-              <tr>
-                <th>MSSV</th>
-                <th>Họ và tên</th>
-                <th>Ban</th>
-              </tr>
-            </thead>
-            <tbody id="listingTable">
-              {data?.data?.map((item: any, index: any) => (
-                <tr key={index}>
-                  <td>{item.username}</td>
-                  <td>{item.name}</td>
-                  <td>{item.major}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <Pagination data={data} onPageChange={onPageChange} />
+    <section className="relative">
+    <div className="btn text-white" onClick={showAddForm}>
+      Thêm Ban
+    </div>
+    {count === 1 && 
+    <form
+      className="modal-box absolute z-[2] fixed"
+      onSubmit={(e) => {e.preventDefault()}}
+    >
+      <h3 className="text-2xl font-semibold pb-4 border-b-2 mb-2">
+        Thêm Role
+      </h3>
+      <div className="form-control  first-letter:max-w-sm pb-5">
+        <label htmlFor="input_committee_name" className="label">
+          <span className="label-text font-semibold text-base">
+            Tên Role
+            <span className="text-[red]">*</span>
+          </span>
+        </label>
+        <input
+          onChange={(e) => setValueRole(e.target.value)}
+          id="input_committee_name"
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered "
+          required
+        />
       </div>
+      <div className="modal-action">
+        <button className="btn" onClick={() => handleAdd(valueRole)}>Thêm</button>
+      </div>
+    </form>
+    }
+    <table className={`table w-full mt-4 text-white z-[1]`}>
+        {/* head */}
+        <thead>
+          <tr>
+            <th>STT</th>
+            <th>Tên role</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+            <th>Hành động</th>
+          </tr>
+        </thead>
+        <tbody>
+        {pageData.data.map((dt: any, index: any) => (
+              <tr key={index}>
+                <th>{dt.id}</th>
+                <td>{dt.name}</td>
+                <td>{dt.created_at}</td>
+                <td>{dt.updated_at}</td>
+                <td>
+                  <div>
+                    <button
+                      className={`btn btn-sm bg-cyan-500 text-white mr-1`}
+                    >
+                      Cập nhật
+                    </button>
+                    <div className="dropdown ">
+                      <button
+                        className={`btn btn-sm bg-error-500 text-white mr-1`}
+                        onClick={showBtn}
+                      >
+                        Xóa
+                      </button>
+                      {num === 1 && <button
+                        className="btn btn-error dropdown-content menu p-2 shadow
+                         text-white rounded-box"
+                         onClick={() => handleDelete(dt.id)}
+                      >
+                        Chắc chắn xóa!
+                      </button>}
+                    </div>
+                  </div>
+                </td>
+              </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination data={pageData} onPageChange={handlePageChange} />
     </section>
-  );
+  )
 }
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {RoleTable()}
+      <TableRole />
     </QueryClientProvider>
   );
 }
